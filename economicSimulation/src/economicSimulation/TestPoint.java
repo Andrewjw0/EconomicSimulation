267ea50181d79bcdc4d70f21303df919d2ee3112
Andrewjw0 <142818280+Andrewjw0@users.noreply.github.com>
2023-08-31 12:05:33 -0500

    Update TestPoint.java
    added test constructors and testing for object equals

--- economicSimulation/src/economicSimulation/TestPoint.java
+++ economicSimulation/src/economicSimulation/TestPoint.java
@@ -1,31 +1,35 @@
 package economicSimulation;
 
-public class TestPoint 
-{
-	private Point p0;
-	private Point p1;
+public class TestPoint {
 	
-	public TestPoint()
+	public void testConstructors()
 	{
-		new Point();
-		Point pt = new Point();
-		System.out.println("Point default coords "+pt.toString());
+		Point p0 = new Point();
+		Point p1 = new Point(2, 3.4);
 		
-		Point op = new Point(0,0.0);
-		System.out.println("is "+op.toString()+" equal to pt? "+pt.equals(op));
-		testEquals();
+		System.out.println("p0 equals" + p0.toString()); // Should be the default, or (0, 0.0)
+		System.out.println("p1 equals" + p1.toString()); // Should be (2, 3.4)
 	}
 	
 	public void testEquals()
 	{
-		p0 = new Point(7,7.0);
-		p1 = new Point(7,7.01);
-		System.out.println("Are p0 "+p0.toString()+" and p1 "+p1.toString()+" equal (or less than a cent in difference)? "+p0.equals(p1));
-	}
-	
-	public static void main(String[] argv)
-	{
-		new TestPoint();
+		Point p0 = new Point(1, 1.0);
+		Point p1 = new Point (1, 1.0);
+		Point p2 = new Point(1, 1.009);
+		Object pobj0 = (Object) p0;
+		Object obj0 = new Object();
 		
+		System.out.println("Are p0 and p1 equal?" + p0.equals(p1)); // They are equal and uses the overLOAD method (true)
+		System.out.println("Are p0 and p2 equal?" + p0.equals(p2)); // They are less than 1 cent apart and uses the overLoad method (true)
+		System.out.println("Are pobj0 and p1 equal?" + pobj0.equals(p1)); // They are equal and uses the overRIDE method (true)
+		System.out.println("Are pobj0 and obj0 equal?" + pobj0.equals(obj0)); // obj0 is not a Point and uses the overRIDE method (false)
 	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestPoint testPoint = new TestPoint();
+		testPoint.testConstructors();
+		testPoint.testEquals();
+	}
+
 }
